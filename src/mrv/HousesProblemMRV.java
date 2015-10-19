267901ddd4bc87af6d762afc326adeb27c6682c9
Akshay Kulkarni <akshaykul13@gmail.com>
2015-10-19 15:21:25 -0500

    Minor Changes

--- src/mrv/HousesProblemMRV.java
+++ src/mrv/HousesProblemMRV.java
@@ -1,6 +1,7 @@
 package mrv;
 import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.HashMap;
 import java.util.PriorityQueue;
 import java.util.Stack;
 
@@ -70,9 +71,11 @@ public class HousesProblemMRV {
 	}
 	
 	private static void printSolution(int[][] solution) {
+		HashMap<Integer, String> map = getVariableNames();
 		for (int i = 0; i < 5; i++) {
 			for (int j = 0; j < 5; j++) {
-				System.out.print(solution[i][j] + " ");
+				System.out.println(map.get(i*5 + j) + " - House Number " + solution[i][j]);
+				//System.out.print(solution[i][j] + " ");
 			}
 		}
 		System.out.println();
@@ -165,103 +168,103 @@ public class HousesProblemMRV {
 			
 			if (duplicate(solution[NATIONALITY]) || duplicate(solution[COLOR]) || duplicate(solution[CHOCOLATE]) 
 					|| duplicate(solution[DRINK]) || duplicate(solution[ANIMAL])) {
-				System.out.println("Duplicate violation");
+				// System.out.println("Duplicate violation");
 				return false;
 			}
 			
 			// The Englishman lives in the red house
 			if (solution[NATIONALITY][ENGLISHMAN] != -1 && solution[COLOR][RED] != -1 
 					&& solution[NATIONALITY][ENGLISHMAN] != solution[COLOR][RED]) {
-				System.out.println("The Englishman lives in the red house");
+				// System.out.println("The Englishman lives in the red house");
 				return false;
 			}
 			
 			// The Spaniard owns the dog
 			if (solution[NATIONALITY][SPANIARD] != -1 && solution[ANIMAL][DOG] != -1 
 					&& solution[NATIONALITY][SPANIARD] != solution[ANIMAL][DOG]) {
-				System.out.println("The Spaniard owns the dog");
+				// System.out.println("The Spaniard owns the dog");
 				return false;
 			}
 			
 			// The Norwegian lives in the first house on the left
 			if (solution[NATIONALITY][NORWEGIAN] != -1 && solution[NATIONALITY][NORWEGIAN] != 0) {
-				System.out.println("The Norwegian lives in the first house on the left");
+				// System.out.println("The Norwegian lives in the first house on the left");
 				return false;
 			}
 			
 			// The green house is immediately to the right of the ivory house
 			if (solution[COLOR][GREEN] != -1 && solution[COLOR][IVORY] != -1 
-					&& Math.abs(solution[COLOR][GREEN] - solution[COLOR][IVORY]) != 1) {
-				System.out.println("The green house is immediately to the right of the ivory house");
+					&& solution[COLOR][GREEN] - solution[COLOR][IVORY] != 1) {
+				// System.out.println("The green house is immediately to the right of the ivory house");
 				return false;
 			}
 			
 			// The man who eats Hershey bars lives in the house next to the man with the fox
 			if (solution[CHOCOLATE][HERSHEYS] != -1 && solution[ANIMAL][FOX] != -1 
 					&& Math.abs(solution[CHOCOLATE][HERSHEYS] - solution[ANIMAL][FOX]) != 1) {
-				System.out.println("The man who eats Hershey bars lives in the house next to the man with the fox");
+				// System.out.println("The man who eats Hershey bars lives in the house next to the man with the fox");
 				return false;
 			}
 			
 			// Kits Kats are eaten in the yellow house
 			if (solution[CHOCOLATE][KITKAT] != -1 && solution[COLOR][YELLOW] != -1 
 					&& solution[CHOCOLATE][KITKAT] != solution[COLOR][YELLOW]) {
-				System.out.println("Kits Kats are eaten in the yellow house");
+				// System.out.println("Kits Kats are eaten in the yellow house");
 				return false;
 			}
 			
 			// The Norwegian lives next to the blue house
 			if (solution[NATIONALITY][NORWEGIAN] != -1 && solution[COLOR][BLUE] != -1 
 					&& Math.abs(solution[NATIONALITY][NORWEGIAN] - solution[COLOR][BLUE]) != 1) {
-				System.out.println("The Norwegian lives next to the blue house");
+				// System.out.println("The Norwegian lives next to the blue house");
 				return false;
 			}
 			
 			// The Smarties eater owns snails
 			if (solution[CHOCOLATE][SMARTIES] != -1 && solution[ANIMAL][SNAIL] != -1 
 					&& solution[CHOCOLATE][SMARTIES] != solution[ANIMAL][SNAIL]) {
-				System.out.println("The Smarties eater owns snails");
+				// System.out.println("The Smarties eater owns snails");
 				return false;
 			}
 			
 			// The Snickers eater drinks orange juice
 			if (solution[CHOCOLATE][SNICKERS] != -1 && solution[DRINK][ORANGEJUICE] != -1 
 					&& solution[CHOCOLATE][SNICKERS] != solution[DRINK][ORANGEJUICE]) {
-				System.out.println("The Snickers eater drinks orange juice");
+				// System.out.println("The Snickers eater drinks orange juice");
 				return false;
 			}
 			
 			// The Ukranian drinks tea
 			if (solution[NATIONALITY][UKRANIAN] != -1 && solution[DRINK][TEA] != -1 
 					&& solution[NATIONALITY][UKRANIAN] != solution[DRINK][TEA]) {
-				System.out.println("The Ukranian drinks tea");
+				// System.out.println("The Ukranian drinks tea");
 				return false;
 			}
 			
 			// The Japanese person eats Milky Ways
 			if (solution[NATIONALITY][JAPANESE] != -1 && solution[CHOCOLATE][MILKYWAY] != -1 
 					&& solution[NATIONALITY][JAPANESE] != solution[CHOCOLATE][MILKYWAY]) {
-				System.out.println("The Japanese person eats Milky Ways");
+				// System.out.println("The Japanese person eats Milky Ways");
 				return false;
 			}
 			
 			// Kit Kats are eaten in a house next to the house where the horse is kept
 			if (solution[CHOCOLATE][KITKAT] != -1 && solution[ANIMAL][HORSE] != -1 
 					&& Math.abs(solution[CHOCOLATE][KITKAT] - solution[ANIMAL][HORSE]) != 1) {
-				System.out.println("Kit Kats are eaten in a house next to the house where the horse is kept");
+				// System.out.println("Kit Kats are eaten in a house next to the house where the horse is kept");
 				return false;
 			}
 			
 			// Coffee is drunk in the green house
 			if (solution[DRINK][COFFEE] != -1 && solution[COLOR][GREEN] != -1 
 					&& solution[DRINK][COFFEE] != solution[COLOR][GREEN]) {
-				System.out.println("Coffee is drunk in the green house");
+				// System.out.println("Coffee is drunk in the green house");
 				return false;
 			}
 			
 			// Milk is drunk in the middle house
 			if (solution[DRINK][MILK] != -1 && solution[DRINK][MILK] != 2) {
-				System.out.println("Milk is drunk in the middle house");
+				// System.out.println("Milk is drunk in the middle house");
 				return false;
 			}		
 			
@@ -277,5 +280,35 @@ public class HousesProblemMRV {
 				}
 			}
 			return false;
+		}
+		
+		private static HashMap<Integer, String> getVariableNames() {
+			HashMap<Integer, String> map = new HashMap<Integer, String>();
+			map.put(NATIONALITY*5 + ENGLISHMAN, "ENGLISHMAN");
+			map.put(NATIONALITY*5 + SPANIARD, "SPANIARD");
+			map.put(NATIONALITY*5 + NORWEGIAN, "NORWEGIAN");
+			map.put(NATIONALITY*5 + UKRANIAN, "UKRANIAN");
+			map.put(NATIONALITY*5 + JAPANESE, "JAPANESE");
+			map.put(COLOR*5 + RED, "RED");
+			map.put(COLOR*5 + GREEN, "GREEN");
+			map.put(COLOR*5 + IVORY, "IVORY");
+			map.put(COLOR*5 + YELLOW, "YELLOW");
+			map.put(COLOR*5 + BLUE, "BLUE");
+			map.put(CHOCOLATE*5 + HERSHEYS, "HERSHEYS");
+			map.put(CHOCOLATE*5 + KITKAT, "KITKAT");
+			map.put(CHOCOLATE*5 + SMARTIES, "SMARTIES");
+			map.put(CHOCOLATE*5 + SNICKERS, "SNICKERS");
+			map.put(CHOCOLATE*5 + MILKYWAY, "MILKYWAY");
+			map.put(DRINK*5 + ORANGEJUICE, "ORANGEJUICE");
+			map.put(DRINK*5 + TEA, "TEA");
+			map.put(DRINK*5 + COFFEE, "COFFEE");
+			map.put(DRINK*5 + MILK, "MILK");
+			map.put(DRINK*5 + WATER, "WATER");
+			map.put(ANIMAL*5 + DOG, "DOG");
+			map.put(ANIMAL*5 + FOX, "FOX");
+			map.put(ANIMAL*5 + SNAIL, "SNAIL");
+			map.put(ANIMAL*5 + HORSE, "HORSE");
+			map.put(ANIMAL*5 + ZEBRA, "ZEBRA");				
+			return map;
 		}
 }
